8fd1b68d5eba5ba75a55edc3dbf996e6a1e1d88d
erick <erick@YAHIR>
2023-11-23 12:47:08 -0600

    Agregar archivos de proyecto.

---
+++ DCOMClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DCOMServer;
+
+namespace DCOMClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IMyInterface serverObject = new MyServer();
+                Console.WriteLine(serverObject.SayHello());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
